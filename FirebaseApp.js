597b6409deb8a44e284d2b6038f7e0a1c684aa4d
Yuk Fai Hsu <yukfai@Yuks-MacBook-Pro.local>
2022-09-26 15:03:54 -0400

    update FirebaseApp.js

--- FirebaseApp.js
+++ FirebaseApp.js
@@ -1,5 +1,18 @@
-import firebaseConfig from './config/firebase-keys'
+// 1. import the firebase project keys
+import firebaseConfig from "./config/firebase-keys";
 
+// other imports from firebase libraries
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+// 2. instantiate the firebase app
+// 3. the "export" keyword enables the firebaseApp variable to be accessible outside this file
 export const firebaseApp = initializeApp(firebaseConfig);
+
+// 4. @TODO: instantiate any other firebase services here
+// 4. instantiate the auth variable
+export const auth = getAuth(firebaseApp);
+
+// - firestore
+export const db = getFirestore(firebaseApp);
