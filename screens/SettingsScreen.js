5646dacff244ad47036e803cce2f2098c8e2b5ae
Morris Wan <kajimone@gmail.com>
2022-10-06 03:05:44 -0400

    modified Settings screen layout

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,10 +1,57 @@
-import { StyleSheet, Text, SafeAreaView,  TextInput, View, Pressable} from 'react-native';
-const SettingsScreen = () => {
-  return (
-    <SafeAreaView>
-        <Text>Settings</Text>
-    </SafeAreaView>
-  )
+import { FlatList, StyleSheet, TouchableOpacity,Text, View, } from "react-native";
+import { FontAwesome } from '@expo/vector-icons';
+
+const SettingsScreen = ( {navigation, route} ) => {
+    const settingList = ['Edit Your Profile'];
+
+    const renderItem = ( {item} ) => (
+        <TouchableOpacity onPress={ () => { navigation.navigate('Edit Your Profile'); } }>
+          <View style={styles.listItem}>
+            <View style={{flexDirection: 'row'}}>
+              <FontAwesome style={styles.profileIcon} name="user-o" size={30}/>
+              <Text style={styles.settingText}> {item} </Text>
+            </View>
+            <FontAwesome name="angle-right" size={30} color="orangered"/>
+          </View>
+        </TouchableOpacity>
+    );
+
+    return (
+        <View style={styles.container}>
+          <FlatList 
+            data={settingList}
+            renderItem={ renderItem }
+          />
+        </View>
+    );
 }
 
-export default SettingsScreen
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    listItem: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10, 
+        borderBottomColor: '#D6D6D6', 
+        borderBottomWidth: 1,
+    },
+    settingText: {
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        fontSize: 25,
+        padding: 5
+    },
+    settingTitle: {
+      flexDirection: 'row',
+  },
+  profileIcon: {
+    padding: 3
+  }
+  });
+
+export default SettingsScreen;
