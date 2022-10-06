116ef21976d7418160b99a9ce0943f88e76de892
Morris Wan <kajimone@gmail.com>
2022-10-06 03:06:20 -0400

    created EditProfileScreen

---
+++ screens/EditProfileScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, SafeAreaView,  TextInput, View, Pressable} from 'react-native';
+const EditProfileScreen = () => {
+  return (
+    <SafeAreaView>
+        <Text>Content of Edit Profile Page</Text>
+    </SafeAreaView>
+  )
+}
+
+export default EditProfileScreen;
