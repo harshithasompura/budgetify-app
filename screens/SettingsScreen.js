ecb0d046f6a2976361b804143bb66b9b2bf82144
Morris Wan <kajimone@gmail.com>
2022-10-13 04:39:52 -0400

    re-structured settingList

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -2,14 +2,14 @@ import { FlatList, StyleSheet, TouchableOpacity,Text, View, } from "react-native
 import { FontAwesome } from '@expo/vector-icons';
 
 const SettingsScreen = ( {navigation, route} ) => {
-    const settingList = ['Edit Your Profile'];
+    const settingList = [{icon: 'user-circle', text: 'Edit Your Profile', screen: 'Edit Your Profile'}];
 
     const renderItem = ( {item} ) => (
-        <TouchableOpacity onPress={ () => { navigation.navigate('Edit Your Profile'); } }>
+        <TouchableOpacity onPress={ () => { navigation.navigate(item.screen); } }>
           <View style={styles.listItem}>
             <View style={{flexDirection: 'row'}}>
-              <FontAwesome style={styles.profileIcon} name="user-o" size={30}/>
-              <Text style={styles.settingText}> {item} </Text>
+              <FontAwesome style={styles.profileIcon} name={item.icon} size={35}/>
+              <Text style={styles.settingText}> {item.text} </Text>
             </View>
             <FontAwesome name="angle-right" size={30} color="orangered"/>
           </View>
@@ -46,12 +46,9 @@ const styles = StyleSheet.create({
         fontSize: 25,
         padding: 5
     },
-    settingTitle: {
-      flexDirection: 'row',
-  },
-  profileIcon: {
-    padding: 3
-  }
-  });
+    profileIcon: {
+      padding: 3
+    }
+});
 
 export default SettingsScreen;
