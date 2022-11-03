a1b940d834172c8318486221a914fcc15f31c051
personal <yfhsu2@gmail.com>
2022-11-03 19:41:05 -0400

    In InputExpensesScreen, expenses are now save to firestore; In ExpensesScreen, the data of Flatlist is fetched from firestore

--- frontend/src/screens/InputExpensesScreen.js
+++ frontend/src/screens/InputExpensesScreen.js
@@ -23,6 +23,10 @@ import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../FirebaseApp";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 
+// Firebase
+import { db } from "../FirebaseApp";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+
 // Importing fonts
 import {
   IBMPlexMono_400Regular,
