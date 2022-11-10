8dddad0e50b6662b5f2a9c2d563ba9f893ff5bd9
Harshitha Sompura <sompharshi123@gmail.com>
2022-11-10 18:49:15 -0500

    Updated ExpensesList using Firebase (#8)
    * add more categories for expenses
    
    * In InputExpensesScreen, expenses are now save to firestore; In ExpensesScreen, the data of Flatlist is fetched from firestore
    
    * add more categories for expenses
    
    * In InputExpensesScreen, expenses are now save to firestore; In ExpensesScreen, the data of Flatlist is fetched from firestore
    
    * use redux to store the expenses
    
    Co-authored-by: personal <yfhsu2@gmail.com>

---
+++ screens/store/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import ExpensesReducer from '../store/Expenses/reducer';
+
+const reducers = combineReducers({
+  Expenses: ExpensesReducer,
+});
+
+export default reducers;
