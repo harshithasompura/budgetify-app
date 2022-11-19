feb1f36f69dd006c624ad72b62e7aec9d88ebba8
h-sompura <sompharshi123@gmail.com>
2022-11-19 15:18:09 -0500

    Conflict Resolve #14

--- src/redux/store/rootSagas.js
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-import expensesSagas from './Expenses/sagas';
-
-function* rootSagas() {
-  yield all([...expensesSagas]);
-}
-
-export default rootSagas;
