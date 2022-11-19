69baedb7cdba1c6542fc8956b8773c930f4b492d
Harshitha Sompura <sompharshi123@gmail.com>
2022-11-19 15:19:07 -0500

    Refactoring FE (#15)
    * Added redux packages
    
    * reorganised Frontend
    
    * Reorganised Front-End
    
    * Conflict Resolve #14

--- src/redux/store/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import ExpensesReducer from '../store/Expenses/reducer';
-
-const reducers = combineReducers({
-  Expenses: ExpensesReducer,
-});
-
-export default reducers;
