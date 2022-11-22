05d7afee3973b4effaf62834e3d834c0fdac4018
Harshitha Sompura <sompharshi123@gmail.com>
2022-11-22 00:32:09 -0500

    Redux issue resolved  (#18)
    * add more categories for expenses
    
    * In InputExpensesScreen, expenses are now save to firestore; In ExpensesScreen, the data of Flatlist is fetched from firestore
    
    * In InputExpensesScreen, expenses are now save to firestore; In ExpensesScreen, the data of Flatlist is fetched from firestore
    
    * use redux to store the expenses
    
    * fix the bug in FirebaseApp.js
    
    * In InputExpensesScreen, expenses are now save to firestore; In ExpensesScreen, the data of Flatlist is fetched from firestore
    
    * In InputExpensesScreen, expenses are now save to firestore; In ExpensesScreen, the data of Flatlist is fetched from firestore
    
    * use redux to store the expenses
    
    * fix the bug in FirebaseApp.js
    
    * fix the bugs in redux
    
    Co-authored-by: personal <yfhsu2@gmail.com>

--- frontend/src/redux/store/Expenses/sagas.js
+++ frontend/src/redux/store/Expenses/sagas.js
@@ -46,93 +46,111 @@ export function* fetchExpenses({ payload }) {
     if (!allExpenses) return;
 
     yield put(
-      actions.setGroceriesExpense(
-        calculateMonthExpense(allExpenses["Groceries"])
-      )
-    );
+        actions.setGroceriesExpense(
+          calculateMonthExpense(allExpenses["Groceries"])
+        )
+    )
+    console.log("Groceries",calculateMonthExpense(allExpenses["Groceries"]));
+    
     yield put(
       actions.setFoodExpense(calculateMonthExpense(allExpenses["Food"]))
     );
+    console.log("Food",calculateMonthExpense(allExpenses["Food"]));
+
     yield put(
       actions.setFuelExpense(calculateMonthExpense(allExpenses["Fuel"]))
     );
+    console.log("Fuel",calculateMonthExpense(allExpenses["Fuel"]));
+
     yield put(
       actions.setTransportationExpense(
         calculateMonthExpense(allExpenses["Transportation"])
       )
     );
+    console.log("Transportation",calculateMonthExpense(allExpenses["Transportation"]));
+
     yield put(
       actions.setEntertainmentExpense(
         calculateMonthExpense(allExpenses["Entertainment"])
       )
     );
+    console.log("Entertainment",calculateMonthExpense(allExpenses["Entertainment"]));
+
     yield put(
       actions.setHousingExpense(calculateMonthExpense(allExpenses["Housing"]))
     );
+    console.log("Housing",calculateMonthExpense(allExpenses["Housing"]));
+
     yield put(
       actions.setClothingExpense(calculateMonthExpense(allExpenses["Clothing"]))
     );
+    console.log("Clothing",calculateMonthExpense(allExpenses["Clothing"]));
+
     yield put(
       actions.setHealthExpense(calculateMonthExpense(allExpenses["Health"]))
     );
+    console.log("Health",calculateMonthExpense(allExpenses["Health"]));
+
     yield put(
       actions.setOthersExpense(calculateMonthExpense(allExpenses["Others"]))
     );
+    console.log("Others",calculateMonthExpense(allExpenses["Others"]));
+
 
     yield put(
       actions.setExpensesData([
         {
           id: "0",
           category: "Groceries",
-          imagePath: require(`../../../assets/expenses/groceries-icon.png`),
+          imagePath: require(`../../../../assets/expenses/groceries-icon.png`),
           expense: calculateMonthExpense(allExpenses["Groceries"]),
         },
         {
           id: "1",
           category: "Food",
-          imagePath: require(`../../../assets/expenses/food-icon.png`),
+          imagePath: require(`../../../../assets/expenses/food-icon.png`),
           expense: calculateMonthExpense(allExpenses["Food"]),
         },
         {
           id: "2",
           category: "Fuel",
-          imagePath: require(`../../../assets/expenses/fuel-icon.png`),
+          imagePath: require(`../../../../assets/expenses/fuel-icon.png`),
           expense: calculateMonthExpense(allExpenses["Fuel"]),
         },
         {
           id: "3",
           category: "Transportation",
-          imagePath: require(`../../../assets/expenses/transportation-icon.png`),
+          imagePath: require(`../../../../assets/expenses/transportation-icon.png`),
           expense: calculateMonthExpense(allExpenses["Transportation"]),
         },
         {
           id: "4",
           category: "Entertainment",
-          imagePath: require(`../../../assets/expenses/entertainment-icon.png`),
+          imagePath: require(`../../../../assets/expenses/entertainment-icon.png`),
           expense: calculateMonthExpense(allExpenses["Entertainment"]),
         },
         {
           id: "5",
           category: "Housing",
-          imagePath: require(`../../../assets/expenses/housing-icon.png`),
+          imagePath: require(`../../../../assets/expenses/housing-icon.png`),
           expense: calculateMonthExpense(allExpenses["Housing"]),
         },
         {
           id: "6",
           category: "Clothing",
-          imagePath: require(`../../../assets/expenses/clothing-icon.png`),
+          imagePath: require(`../../../../assets/expenses/clothing-icon.png`),
           expense: calculateMonthExpense(allExpenses["Clothing"]),
         },
         {
           id: "7",
           category: "Health",
-          imagePath: require(`../../../assets/expenses/health-icon.png`),
+          imagePath: require(`../../../../assets/expenses/health-icon.png`),
           expense: calculateMonthExpense(allExpenses["Health"]),
         },
         {
           id: "8",
           category: "Others",
-          imagePath: require(`../../../assets/expenses/others-icon.png`),
+          imagePath: require(`../../../../assets/expenses/others-icon.png`),
           expense: calculateMonthExpense(allExpenses["Others"]),
         },
       ])
