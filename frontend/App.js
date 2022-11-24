737e82d77f296c6230db7ffdc36fa976b90cfd91
h-sompura <sompharshi123@gmail.com>
2022-11-24 06:54:27 -0500

    Ignored AsyncStorage Warnings

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { LogBox } from "react-native";
 // Navigation imports
 import { NavigationContainer, CommonActions } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -21,16 +22,16 @@ import { signOut } from "firebase/auth";
 import { onAuthStateChanged } from "firebase/auth";
 
 //Screen Imports
-import HomeScreen from "./src/screens/HomeScreen"
-import LoginScreen from "./src/screens/LoginScreen"
+import HomeScreen from "./src/screens/HomeScreen";
+import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
 import ExpensesScreen from "./src/screens/ExpensesScreen";
 import TabController from "./src/screens/TabController";
 import ForgotPasswordScreen from "./src/screens/ForgotPasswordScreen";
 
 // Redux
-import { Provider as ReduxProvider } from 'react-redux';
-import store from './src/redux/store';
+import { Provider as ReduxProvider } from "react-redux";
+import store from "./src/redux/store";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -62,77 +63,79 @@ export default function App() {
   if (!fontsLoaded) {
     return;
   } else {
+    LogBox.ignoreLogs([
+      "AsyncStorage has been extracted from react-native core",
+    ]);
+    // ------------------------ View Template -----------------------
+    return (
+      <ReduxProvider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+              name="Tab"
+              component={TabController}
+              options={({ navigation }) => ({
+                headerStyle: {
+                  backgroundColor: "#001C00",
+                },
+                headerTitleStyle: {
+                  fontWeight: "bold",
+                },
+                headerBackVisible: false,
+                headerLeft: () => null,
+                headerRight: () => (
+                  <Button
+                    onPress={async () => {
+                      try {
+                        await signOut(auth);
+                        console.log("User signed out");
 
-  // ------------------------ View Template -----------------------
-  return (
-    <ReduxProvider store={store}>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Tab"
-          component={TabController}
-          options={({ navigation }) => ({
-            headerStyle: {
-              backgroundColor: "#001C00",
-            },
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-            headerBackVisible: false,
-            headerLeft: () => null,
-            headerRight: () => (
-              <Button
-                onPress={async () => {
-                  try {
-                    await signOut(auth);
-                    console.log("User signed out");
-
-                    //reset the navigation state after logged out
-                    navigation.dispatch(
-                      CommonActions.reset({
-                        index: 0,
-                        routes: [{ name: "Login" }],
-                      })
-                    );
-                  } catch (err) {
-                    console.log(err);
-                  }
-                }}
-                title="Sign out"
-                color="#C5F277"
-              />
-            ),
-          })}
-        />
-        <Stack.Screen
-          options={{
-            headerShown: false, // change this to `false`
-          }}
-          name="Register"
-          component={RegisterScreen}
-        />
-        <Stack.Screen
-          name="Login"
-          options={{
-            headerShown: false, // change this to `false`
-          }}
-          component={LoginScreen}
-        />
-        <Stack.Screen
-          name="ForgotPassword"
-          options={{
-            headerTintColor: "#B17BFF",
-            headerTitle: "",
-            headerShadowVisible: false, // applied here
-            headerStyle: {
-              backgroundColor: "#F2F3F4",
-            },
-          }}
-          component={ForgotPasswordScreen}
-        />
-        <Stack.Screen name="Expenses" component={ExpensesScreen} />
-      </Stack.Navigator>
-      </NavigationContainer>
+                        //reset the navigation state after logged out
+                        navigation.dispatch(
+                          CommonActions.reset({
+                            index: 0,
+                            routes: [{ name: "Login" }],
+                          })
+                        );
+                      } catch (err) {
+                        console.log(err);
+                      }
+                    }}
+                    title="Sign out"
+                    color="#C5F277"
+                  />
+                ),
+              })}
+            />
+            <Stack.Screen
+              options={{
+                headerShown: false, // change this to `false`
+              }}
+              name="Register"
+              component={RegisterScreen}
+            />
+            <Stack.Screen
+              name="Login"
+              options={{
+                headerShown: false, // change this to `false`
+              }}
+              component={LoginScreen}
+            />
+            <Stack.Screen
+              name="ForgotPassword"
+              options={{
+                headerTintColor: "#B17BFF",
+                headerTitle: "",
+                headerShadowVisible: false, // applied here
+                headerStyle: {
+                  backgroundColor: "#F2F3F4",
+                },
+              }}
+              component={ForgotPasswordScreen}
+            />
+            <Stack.Screen name="Expenses" component={ExpensesScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </ReduxProvider>
     );
   }
