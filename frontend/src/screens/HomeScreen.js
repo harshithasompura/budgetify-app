1d3da3e32bfc65e6952d3592ba8c01b1349667ca
h-sompura <sompharshi123@gmail.com>
2022-11-24 19:28:07 -0500

    Analytics - Individual Progress Expenses Added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -11,7 +11,7 @@ import {
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import expensesSelectors from "../redux/store/Expenses/selectors";
-
+import * as Progress from "react-native-progress";
 import { PieChart, ProgressChart } from "react-native-chart-kit";
 const HomeScreen = () => {
   const width = Dimensions.get("window").width;
@@ -42,6 +42,14 @@ const HomeScreen = () => {
   const otherExpensesFromFirebase = useSelector(
     expensesSelectors.getOthersExpense
   );
+
+  const allExpenseFromFirebase = useSelector(
+    expensesSelectors.getExpensesData
+  )
+
+  const budget = useSelector(
+    expensesSelectors.getBudget
+  )
 
   const pieChartData = [
     {
@@ -120,6 +128,7 @@ const HomeScreen = () => {
       >
         Analytics
       </Text>
+      
       <View style={styles.pieChartContainer}>
         <PieChart
           data={pieChartData}
@@ -139,6 +148,165 @@ const HomeScreen = () => {
                
               /> */}
       </View>
+      {/* Detailed Overview */}
+      <ScrollView style={styles.expenseContainer}>
+        <View>
+          <Text style={{ fontFamily: "Montserrat_600SemiBold", fontSize:16, marginVertical:10 }}>Total Expenses</Text>
+          <Text>{}</Text>
+        </View>
+        <View>
+        {/* Individial Progress */}
+        <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+        <Text style={{ fontFamily: "Montserrat_400Regular", fontSize:15, marginVertical:8, }}>Groceries</Text>
+        <Text style={{ fontFamily: "Montserrat_600SemiBold", fontSize:15, marginVertical:8, }}>${groceryExpensesFromFirebase}</Text>
+        </View>
+        <Progress.Bar
+              progress={groceryExpensesFromFirebase / budget}
+              width={null}
+              height={8}
+              color={
+                "#5844FB" 
+              }
+              unfilledColor={
+               "#fff"
+              }
+              borderRadius={20}
+            />
+        </View>
+        <View>
+        {/* Individial Progress */}
+        <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+        <Text style={{ fontFamily: "Montserrat_400Regular", fontSize:15, marginVertical:8, }}>Food</Text>
+        <Text style={{ fontFamily: "Montserrat_600SemiBold", fontSize:15, marginVertical:8, }}>${foodExpensesFromFirebase}</Text>
+        </View>
+        <Progress.Bar
+              progress={foodExpensesFromFirebase / budget}
+              width={null}
+              height={8}
+              color={
+                "#FBE901" 
+              }
+              unfilledColor={
+               "#fff"
+              }
+              borderRadius={20}
+            />
+        </View>
+        <View>
+        {/* Individial Progress */}
+        <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+        <Text style={{ fontFamily: "Montserrat_400Regular", fontSize:15, marginVertical:8, }}>Fuel</Text>
+        <Text style={{ fontFamily: "Montserrat_600SemiBold", fontSize:15, marginVertical:8, }}>${fuelExpensesFromFirebase}</Text>
+        </View>
+        <Progress.Bar
+              progress={fuelExpensesFromFirebase / budget}
+              width={null}
+              height={8}
+              color={
+                "#86D3C6" 
+              }
+              unfilledColor={
+               "#fff"
+              }
+              borderRadius={20}
+            />
+        </View>
+        <View>
+        {/* Individial Progress */}
+        <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+        <Text style={{ fontFamily: "Montserrat_400Regular", fontSize:15, marginVertical:8, }}>Transportation</Text>
+        <Text style={{ fontFamily: "Montserrat_600SemiBold", fontSize:15, marginVertical:8, }}>${transportExpensesFromFirebase}</Text>
+        </View>
+        <Progress.Bar
+              progress={transportExpensesFromFirebase / budget}
+              width={null}
+              height={8}
+              color={
+                "#EE4D46" 
+              }
+              unfilledColor={
+               "#fff"
+              }
+              borderRadius={20}
+            />
+        </View>
+        <View>
+        {/* Individial Progress */}
+        <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+        <Text style={{ fontFamily: "Montserrat_400Regular", fontSize:15, marginVertical:8, }}>Entertainment</Text>
+        <Text style={{ fontFamily: "Montserrat_600SemiBold", fontSize:15, marginVertical:8, }}>${entertainExpensesFromFirebase}</Text>
+        </View>
+        <Progress.Bar
+              progress={entertainExpensesFromFirebase / budget}
+              width={null}
+              height={8}
+              color={
+                "#FD8931" 
+              }
+              unfilledColor={
+               "#fff"
+              }
+              borderRadius={20}
+            />
+        </View>
+        <View>
+        {/* Individial Progress */}
+        <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+        <Text style={{ fontFamily: "Montserrat_400Regular", fontSize:15, marginVertical:8, }}>Housing</Text>
+        <Text style={{ fontFamily: "Montserrat_600SemiBold", fontSize:15, marginVertical:8, }}>${housingExpensesFromFirebase}</Text>
+        </View>
+        <Progress.Bar
+              progress={housingExpensesFromFirebase / budget}
+              width={null}
+              height={8}
+              color={
+                "#F9C0FF" 
+              }
+              unfilledColor={
+               "#fff"
+              }
+              borderRadius={20}
+            />
+        </View>
+        <View>
+        {/* Individial Progress */}
+        <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+        <Text style={{ fontFamily: "Montserrat_400Regular", fontSize:15, marginVertical:8, }}>Clothes</Text>
+        <Text style={{ fontFamily: "Montserrat_600SemiBold", fontSize:15, marginVertical:8, }}>${clothesExpensesFromFirebase}</Text>
+        </View>
+        <Progress.Bar
+              progress={clothesExpensesFromFirebase / budget}
+              width={null}
+              height={8}
+              color={
+                "#6FCE25" 
+              }
+              unfilledColor={
+               "#fff"
+              }
+              borderRadius={20}
+            />
+        </View>
+        <View>
+        {/* Individial Progress */}
+        <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+        <Text style={{ fontFamily: "Montserrat_400Regular", fontSize:15, marginVertical:8, }}>Others</Text>
+        <Text style={{ fontFamily: "Montserrat_600SemiBold", fontSize:15, marginVertical:8, }}>${otherExpensesFromFirebase}</Text>
+        </View>
+        <Progress.Bar
+              progress={otherExpensesFromFirebase / budget}
+              width={null}
+              height={8}
+              color={
+                "#202022" 
+              }
+              unfilledColor={
+               "#fff"
+              }
+              borderRadius={20}
+            />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -157,6 +325,21 @@ const styles = StyleSheet.create({
     margin: 20,
     width: "100%",
   },
+  expenseContainer:{
+    backgroundColor:"white",
+    height:"auto",
+    width:"100%",
+    padding:20,
+    borderColor:"black",
+    borderLeftWidth:2,
+    borderTopWidth:1,
+    borderRightWidth:2,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOffset: { width: -2, height: 3 },
+    shadowOpacity: 0.16,
+    shadowRadius: 4,
+  }
 });
 
 export default HomeScreen;
