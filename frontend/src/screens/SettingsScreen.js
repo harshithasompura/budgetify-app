ae0cbf2927f171feb44ed6d23e1da1cb62e092e2
Harshitha Sompura <sompharshi123@gmail.com>
2022-12-07 00:20:46 -0500

    Alert message updates + Removed Manage Categories (#41)
    * Added destructive style to Delete Alerts
    
    * Removed Manage Categories

--- frontend/src/screens/SettingsScreen.js
+++ frontend/src/screens/SettingsScreen.js
@@ -52,10 +52,10 @@ const SettingsScreen = ({ navigation, route }) => {
   ];
 
   const preferencesList = [
-    {
-      text: "Manage Categories",
-      screen: "ManageCategories",
-    },
+    // {
+    //   text: "Manage Categories",
+    //   screen: "ManageCategories",
+    // },
     {
       text: "Terms & Conditions",
       url: termsURL,
@@ -81,7 +81,8 @@ const SettingsScreen = ({ navigation, route }) => {
         style: "cancel",
       },
       {
-        text: "OK",
+        text: "Delete",
+        style: "destructive",
         onPress: () => {
           console.log("OK Pressed");
           const listener = onAuthStateChanged(auth, async (user) => {
